b2ce8c9f6933e2607863c474591974723eab4ba6
Ekete Emmanuel <eketeeu@gmail.com>
2023-02-15 22:13:43 +0100

    initial commit

---
+++ build/precache-manifest.08525baa102cda13eb4dec1962a396f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13480300c97c0e4a1e5c1946d0c55fd2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95f33aa92b12edb7b182",
+    "url": "/static/css/main.cb917dac.chunk.css"
+  },
+  {
+    "revision": "fba166c969662052c92e",
+    "url": "/static/js/2.d1887339.chunk.js"
+  },
+  {
+    "revision": "95f33aa92b12edb7b182",
+    "url": "/static/js/main.0f158318.chunk.js"
+  },
+  {
+    "revision": "f67c199efb9f2c29f389",
+    "url": "/static/js/runtime-main.f294effb.js"
+  }
+]);
